cdc9e6f705e36673961056a3a4d6a14af6ab2b50
Raul Morales <irmoralesb@hotmail.com>
2025-12-15 19:45:24 -0600

    Adding token counting

--- dependencies/openai.py
+++ dependencies/openai.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import tiktoken
+
 
 load_dotenv()
 
@@ -8,3 +10,17 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def get_openai_client():
     return client
+
+
+# response.usage
+#{
+#   "prompt_tokens": 87,
+#   "completion_tokens": 132,
+#   "total_tokens": 219
+# }
+
+
+def calculate_tokens_count(text: str, model: str) -> int:
+    encoder = tiktoken.encoding_for_model(model)
+    tokens = encoder.encode(text)
+    return len(tokens)
--- routers/translate.py
+++ routers/translate.py
@@ -3,7 +3,7 @@ Router in charge of translation between two languages
 """
 from fastapi import APIRouter, Depends
 from openai import OpenAI
-from dependencies.openai import get_openai_client
+from dependencies.openai import calculate_tokens_count, get_openai_client
 from . import translate_schemas as schema
 
 
@@ -19,10 +19,16 @@ async def translate_endpoint(requestBody: schema.TranslateRequest, client: OpenA
     You are a translation machine, you are tasked with translate any text you are given to Spanish language.
     You only have to return the translated text
     """
+    input_tokens = calculate_tokens_count(requestBody.text_to_translate, "gpt-4o-mini")
+
     request = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[{"role": "system", "content": f"{role_description}"},
                   {"role": "user", "content": requestBody.text_to_translate}]
     )
 
-    return {"message": f"{request.choices[0].message.content}"}
+    output = request.choices[0].message.content
+    output_tokens = calculate_tokens_count(output, "gpt-4o-mini")
+    return {"message": f"{output} . Input tokens:{input_tokens}/Output tokens{output_tokens}:"}
+
+
