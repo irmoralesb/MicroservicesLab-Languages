a64827365cc081bdc6dde1e96f9eba29613c9e40
Raul Morales <irmoralesb@hotmail.com>
2025-12-17 20:16:52 -0600

    Adding return type and put method

--- routers/translate.py
+++ routers/translate.py
@@ -1,7 +1,7 @@
 """
 Router in charge of translation between two languages
 """
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from openai import OpenAI
@@ -10,6 +10,7 @@ from dependencies.openai import calculate_tokens_count, get_openai_client
 from . import translate_schemas as schema
 from databases.database import SessionLocal
 from databases.models import TokenData
+from typing import List
 
 router = APIRouter(
     prefix="/api/v1/translate",
@@ -25,11 +26,18 @@ def get_db():
         db.close()
 
 
-@router.get("/{id}")
-async def get_translation(id: int, db: Session = Depends(get_db)):
+@router.get("/", response_model=List[schema.DisplayTokenData])
+def get_all_translations(db: Session = Depends(get_db)):
+    token_list = db.query(TokenData).all()
+    return token_list
+
+
+# The response model is used to filter the options to show!!!!!
+@router.get("/{id}", response_model=schema.DisplayTokenData)
+async def get_translation(id: int, response: Response, db: Session = Depends(get_db)):
     token_data = db.query(TokenData).filter(TokenData.id == id).first()
     if not token_data:
-        raise HTTPException(status_code=404, detail="Translation not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Translation not found")
 
     return token_data
 
@@ -42,7 +50,7 @@ async def delete_translation(id: int, db: Session = Depends(get_db)):
     return HTTP_202_ACCEPTED
 
 
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def translate_endpoint(requestBody: schema.TranslateRequest, client: OpenAI = Depends(get_openai_client), db: Session = Depends(get_db)):
     role_description = """
     You are a translation machine, you are tasked with translate any text you are given to Spanish language.
@@ -72,3 +80,19 @@ async def translate_endpoint(requestBody: schema.TranslateRequest, client: OpenA
     db.refresh(new_token_data)
 
     return {"message": f"{output} . Input tokens:{input_tokens}/Output tokens{output_tokens}:"}
+
+
+@router.put("/{id}")
+def update(id: int, requestBody: schema.TokenDataRequest, db: Session = Depends(get_db)):
+    token_data_db = db.query(TokenData).filter(TokenData.id == id).first()
+    if not token_data_db:
+        raise HTTPException(status_code=404, detail="Not found")
+
+    token_data_db.input_text = requestBody.input_text
+    token_data_db.input_token_count = requestBody.input_token_count
+    token_data_db.output_text = requestBody.output_text
+    token_data_db.output_token_count = requestBody.output_token_count
+
+    db.commit()
+    db.refresh(token_data_db)
+    return {'Product successfully updated'}
--- routers/translate_schemas.py
+++ routers/translate_schemas.py
@@ -1,5 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class TranslateRequest(BaseModel):
     text_to_translate: str
+
+
+class TokenDataRequest(BaseModel):
+    id: int
+    input_text: str
+    input_token_count: int
+    output_text: str
+    output_token_count: int
+
+
+class DisplayTokenData(BaseModel):
+    input_text: str
+    output_text: str
+    model_config = ConfigDict(from_attributes=True)
